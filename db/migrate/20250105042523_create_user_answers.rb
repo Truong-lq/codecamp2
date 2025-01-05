af49311f62aa860e8e647e33264cdb5680683f9a
Truong-lq <truonglq@zigexn.vn>
2025-01-05 12:31:22 +0700

    init model (#17)

---
+++ db/migrate/20250105042523_create_user_answers.rb
@@ -0,0 +1,11 @@
+class CreateUserAnswers < ActiveRecord::Migration[7.2]
+  def change
+    create_table :user_answers do |t|
+      t.references :question, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+      t.references :answer, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
