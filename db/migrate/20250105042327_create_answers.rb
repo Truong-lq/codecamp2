7c833c0190e5657169348aa19db981832f44b642
Truong-lq <truonglq@zigexn.vn>
2025-01-17 09:28:58 +0700

    feat: create test

--- db/migrate/20250105042327_create_answers.rb
+++ db/migrate/20250105042327_create_answers.rb
@@ -1,7 +1,7 @@
 class CreateAnswers < ActiveRecord::Migration[7.2]
   def change
     create_table :answers do |t|
-      t.boolean :correct
+      t.boolean :is_correct, default: false
       t.text :content
       t.references :question, null: false, foreign_key: true
 
--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema[7.2].define(version: 2025_01_05_042523) do
   create_table "answers", force: :cascade do |t|
-    t.boolean "correct"
+    t.boolean "is_correct", default: false
     t.text "content"
     t.integer "question_id", null: false
     t.datetime "created_at", null: false
