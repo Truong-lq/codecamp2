c8832048e1114b9eccf9aa55fa4668d1ec368db2
Truong-lq <truonglq@zigexn.vn>
2025-01-23 14:37:55 +0700

    feat: show test in user view (#22)
    * Work has started on the feat/user_do_test
    
    * feat: show test in user view

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,7 +1,15 @@
 class TestsController < ApplicationController
+  before_action :load_test
+
   def show
   end
 
   def do
+  end
+
+  private
+
+  def load_test
+    @test = Test.find_by id: params[:id]
   end
 end
